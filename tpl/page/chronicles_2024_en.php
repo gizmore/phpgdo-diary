6c3ad4e15a86f3c146b83c990f49d562d0cbebf6
gizmore <gizmore@wechall.net>
2024-01-01 18:48:07 +0100

    'sync'

--- tpl/page/chronicles_2024_en.php
+++ tpl/page/chronicles_2024_en.php
@@ -1,3 +1,74 @@
 11:00 - Ich komme zuhause an... bissel progen und an ESL weiter basteln.
         Toll wäre, wenn ich es schaffe das Direkte Demokratie Thema heute einzupflegen.
 
+13:00 - Ich programmiere fleissig, im Internet läuft eine Radioshow.
+        Ich warne schon einmal vor, meine Mutter zu grüßen, die bald kommt.
+        Ich durchsuche change.org nach direkter Demokratie, und finde eine Petition die meiner nahe kommt.
+        Diese hat 1500 Unterzeichner, und ich verlinke sie in meinem ESL Projekt.
+        Mutti kommt vorbei, und bemängelt, dass ich das Geschirr nicht abgewaschen habe,
+        dabei habe ich alles schon sortiert und vorbereitet.
+        Mein Gruß über's Radio kommt und sie freut sich.
+        Die Zeit wird langsam knapp und ich lade gegen 17:00 die neuste Version hoch.
+        Auf dem Handy sieht es grauenhaft aus... kein Release heute.
+
+18:00 - Wir trudeln bei meinem Bruder DejaVu ein.
+        Es gibt Raclette, wir futtern und quatschen.
+        Wir sind nur zu viert; Mutti, DejaVu, seine Freundin und ich.
+        Um 20:00 kommt Mutti's Freund Hotti vorbei.
+        Er bleibt aber nur kurz.
+        Wir fangen an ein Gesellschaftsspiel zu spielen; Die Siedler von Catan.
+        Es macht Spaß und Mutti gewinnt.
+        DejaVu und seine Freundin necken sich ordentlich ^^ Süß!
+
+23:00 - Es wird Zeit die Neujahrsgrüße vorzubereiten.
+        Ich will einen kleinen Reim auf Deutsch via WhatsApp schicken,
+        sowie eine kurze Nachricht in's IRC und WeChall.net.
+
+        Meine Englische Nachricht für das Internet lautet:
+
+Don't give up, together we can and must improve something in this world, so that we can all live in a peaceful and healthy world. For us, for others and also for the next generation,
+support e.g. Change.org - Direct Democracy in Germany.
+
+        Was die direkte Demokratie Petition verlinkt.
+
+        Auf Deutsch reime ich folgendes, auch an die um ESL zu unterstützen:
+
+Ich sende Grüße, neues Jahr.
+Und egal wie's letzte war,
+das neue können wir bewegen.
+Bitte hilf uns auf den Wegen.
+Damit alle glücklich werden,
+Mensch und Tier auf dieser Erden.....
+
+Frohes neues Jahr 2024.
+
+24:00 - Ich schicke die Grüße gleichzeitig gegen Mitternacht ab.
+        Wir gehen das Feuerwerk ansehen, und spielen ein weiteres Spiel; StarQuest
+        Mein Bruder spielt die Bösewichte für unser Weltraum-Marine-Team und macht sich keine Freunde.
+        Es zieht sich hin bis 03:30, und wir können die Mission, obwohl es die 1. war, nur knapp erledigen.
+
+04:00 - Zu Hause angekommen gehe ich gleich in's Bett.
+        Tabletten habe ich keine Vorrätig, obwohl ich das anders in Erinnerung habe,
+        kann aber gut einschlafen.
+        Silvester ist überstanden :)
+
+===========================================================================================
+
+Mo, 1.Jan.2024
+
+14:00 - Ich habe lange geschlafen.
+        Zwischendurch zweimal aufgewacht, und habe geträumt ich würde Tischtennis im Verein spielen.
+        Mein Schläger wurde geklaut, und habe nur einen Kaputten.
+        Beim Punktspiel muss ich gebückt spielen, weil die Gebäudedecke zu niedrig ist.
+        Naja... was das wohl bedeuten soll.
+
+        Mutti und ich "Frühstücken", danach gehe ich Baden.
+        Ich sende noch einige Neujahrsgrüße und überlege wie es mit ESL weiter geht.
+        Mutti kocht Gulasch.
+
+18:00 - Mittagessen. Etwas spät aber gut.
+        Danach repariere ich noch ein paar Kleinigkeiten im ESL,
+        und ab geht's zum Bahnhof.
+        Vorher noch bissel Zahnpflege.
+
+
