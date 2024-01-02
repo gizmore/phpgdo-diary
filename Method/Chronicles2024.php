f3992e5fde1c9a565fa03b11de964ee11a8fa571
gizmore <gizmore@wechall.net>
2024-01-02 17:22:24 +0100

    2024

---
+++ Method/Chronicles2024.php
@@ -0,0 +1,9 @@
+<?php
+namespace GDO\Diary\Method;
+
+use GDO\UI\MethodPage;
+
+final class Chronicles2024 extends MethodPage
+{
+
+}
---
+++ tpl/page/chronicles2024_en.php
@@ -0,0 +1,53 @@
+Mo, 1.Jan.2024
+
+14:00 - Ich habe lange geschlafen.
+        Zwischendurch zweimal aufgewacht, und habe geträumt ich würde Tischtennis im Verein spielen.
+        Mein Schläger wurde geklaut, und habe nur einen Kaputten.
+        Beim Punktspiel muss ich gebückt spielen, weil die Gebäudedecke zu niedrig ist.
+        Naja... was das wohl bedeuten soll.
+
+        Mutti und ich "Frühstücken", danach gehe ich Baden.
+        Ich sende noch einige Neujahrsgrüße und überlege wie es mit ESL weiter geht.
+        Mutti kocht Gulasch.
+
+18:00 - Mittagessen. Etwas spät aber gut.
+        Danach repariere ich noch ein paar Kleinigkeiten im ESL,
+        und ab geht's zum Bahnhof.
+        Vorher noch bissel Zahnpflege.
+
+21:00 - Ich komme im Erlengrund an.
+        Mein Tischtennispartner will noch ein Match, und eine Stunde spielen
+        Danach gleich in's Bett
+
+===============================================================================
+
+Tue, 2.Jan.2024
+
+06:30 - Die Nacht ist vorbei und ich mag noch nicht aufstehen.
+        Ich bin etwas enttäuscht, das die bestehende Petition die ich gefunden habe
+        nur 1500 Unterschriften generiert hat.
+        Auch weiß ich nicht, wie wir 50,000 zusammenbekommen wollen.
+
+        Der Tag morgens ist gut gefüllt.
+        Ergo, Gruppentherapie, Einzelgespräch mit dem Psychologen, Visite beim Oberarzt.
+        Der Oberarzt ist ein fairer Mensch.
+        Ich erzähle ihm von meinem Problem mit ESL, und er meint ich solle mich auf mich konzentrieren.
+        Meine Therapie läuft aber eigentlich ganz gut.
+        Die Medis reduziert er mir, so wie ich es geplant habe.
+        Allerdings will ich irgendwie auch irgendwas nehmen... Irgendwas.
+
+        Ich mache einen Mittagsschlaf.
+
+14:30 - Planungskonferenz.
+        Meine Therapeutin und eine andere Therapeutin besprechen mit mir meine Therapieziele.
+        Sie meinen, ich müsse meine Einstellung zum Kiffen ändern,
+        da die Gefahr hoch ist, dann auch wieder Alkohol zu trinken.
+
+        Ich erhalte Post von der deutschen Rentenversicherung.
+        Meine Kontonummer für das Übergangsgeld ist falsch. *gnarf*
+
+        Mein Zimmerkumpel und ich räumen auf.
+        Ich trage diesen Bericht nach und arbeite weiter an ESL.
+
+17:30 - Abendessen.
+
