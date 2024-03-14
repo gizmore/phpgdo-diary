1585b5e596081d09d357df87952021e233729829
gizmore <gizmore@wechall.net>
2024-03-14 22:57:43 +0100

    '7.0.3-r1823'

---
+++ tpl/page/chronicles20242_en.php
@@ -0,0 +1,159 @@
+Ich wurde aus der Therapie entlassen.
+
+# Wed, 6.Mar.2024
+
+09:15 - Wieder mal verpennt. Ich habe einen Termin verschlafen.
+        Meine Ansprechpartnerin vom Lukaswerk hat angerufen, ich rufe zurück.
+        Wir verabreden uns zu gleich, ich komme halt etwas später.
+
+10:00 - Gespräch mit meiner Ansprechpartnerin
+        Ich erzähle von meinem Alkoholversuch gestern,
+        und erwähne auch, dass ich weiterhin gelegentlich THC rauchen will.
+        Wir vertagen die Aufnahme für meine ambulante Therapie,
+        da ich für die dauer von 6 Monaten abstinent bleiben müsste.
+        Ich will in erster Linie vom Alkohol weg...
+        Wir einigen uns darauf ersteinmal abzuwarten,
+        und dass wenn ich wieder Alkohol konsumieren würde, ich die ambulante Therapie machen werde.
+
+11:30 - Wieder zuhause.
+        Ich trage diesen Bericht nach, Marvin kommt vorbei, wir gehen einkaufen.
+
+13:30 - Ich widme mich ein wenig meinem Antrag auf Arbeitslosengeld.
+
+16:00 - Ich gehe noch einmal einkaufen.
+        Cola, Bier für Hotti, Kaffee.
+
+17:00 - Ich arbeite an dem neuen Luca Project. Geld habe ich noch nicht gesehen,
+        aber die Arbeit ist wieder ein toller Showcase für mein phpgdo Framework.
+        Es geht um automatisch generierte Formulare.
+
+18:30 - Ich mache Hotti und mir ein paar Baguettes. Mutti will nichts.
+        Weiter geht es daran die Formulare zu verbessern.
+
+21:00 - Mutti und Hotti fahren los.
+        Endlich ruhe für meine Arbeit.
+
+22:00 - YouTube, Medis ab in's Bett
+
+=================================================================================================
+
+# Thu, 7.Mar.2024
+
+09:30 - Ich stehe recht früh auf, koche Kaffee und gehe Baden.
+
+10:30 - Ich gehe zum Frisör, für das Bewerbungsbild in meinem Lebenslauf.
+        Mein Frisör freut sich, mich nach der Reha wiederzusehen.
+        Nach etwas Wartezeit komme ich an die Reihe.
+        Wir rauchen erstmal eine.
+        Er meint er würde meiner Mutter gratis die Haare schneiden,
+        und auch gerne mal wieder mit mir in's Schwimmbad.
+        Wir verabreden uns für Samstag.
+
+12:00 - Ich gehe zum Fotografen. Dieser macht wohl grad Pause, und ich hole mir einen Snack beim Edeka.
+        Suchtdruck habe ich keinen mehr.
+        Der Fotograf ist nun anzutreffen, und wir nehmen einige Fotos auf.
+        Lächeln fällt mir irgendwie etwas schwer, wegen meiner Zähne, die aber garnicht so schlimm aussehen sollten.
+        Naja, die beste Aufnahme schickt er mir per Mail.
+
+13:00 - Ich bin wieder zuhause und warte auf Mutti und Hotti
+
+14:00 - Hotti und Mutti renovieren, ich widme mich wieder meinem Lebenslauf.
+        Ich nehme eine kostenpflichtige Vorlage und baue diese per Hand mit PHP TCPDF nach.
+        Etwas aufwändig und frickelig, dafür komme ich aber voran und später zu einem guten Ergebnis.
+
+20:00 - Wir quatschen noch ein wenig in der Küche, dann verlassen mich die beiden wieder.
+        Ich freue mich schon auf's Bett, nehme Medis und schlafe zu YouTube ein.
+
+=======================================================================================================
+
+# Fri, 8.Mar.2024
+
+09:00 - Wieder früh aufgewacht und motiviert.
+        Ich arbeite weiter am Lebenslauf.
+
+11:00 - Ich treffe mich mit Marvin und wir gehen einkaufen.
+        Ich hole mir ein paar Brötchen, Wurst, eine Tafel Schokolade und Getränke.
+
+13:00 - Weiter geht's am Lebenslauf... es fehlt fast nur noch das Berwerbungsfoto.
+
+14:00 - Mutti und Hotti kommen mit meinem neuen Teppich.
+        Hotti und ich tragen die 5m Rolle hoch.
+
+15:00 - Wir verlgen den Teppich grob.
+        Hotti macht alleine weiter.
+
+16:00 - Ich stelle meinen Lebenslauf nun fertig, lade ihn auf Instaffo hoch und bewerbe mich auf zwei Stellenangebote.
+        Hotti verlegt den Teppich weiter
+
+22:00 - Hotti und Mutti verlassen mich, ich mache mir zwei Baguettes.
+        Ich fühle mich erschöpft, kann aber nur schlecht einschlafen.
+
+
+===================================================================================================
+
+# Sat, 9.Mar.2024
+
+09:00 - Ich stehe früh auf, werkel ein wenig am Luca Projekt.
+
+15:00 - Mutti und Hotti kommen.
+        Hotti übernimmt die letzten Arbeiten am Teppich und der Heizung.
+
+17:00 - Ich gehe uns eine Pizza holen, und auch zwei Bier für Hotti.
+        Verlangen nach Alkohol habe ich nicht.
+
+20:00 - Wir essen Pizza und die beiden verlassen mich.
+
+21:00 - Medis und ab in's Bett.
+
+
+===================================================================================================
+
+# Sun, 10.Mar.2024
+
+09:30 - Ich werde wach, lege mich aber nochmal hin... *ZAP*
+
+15:00 - Hotti und Mutti kommen.
+        Wir trinken Kaffee und warten auf meinen Bruder.
+
+16:00 - Brüderle kommt. Er nimmt einen Schrank auseinander und baut ihn in meinem Zimmer wieder zusammen.
+        Eine große Hilfe bin ich nicht... Ich verhebe mich noch doof bei einem Teil.
+        Marvin und Lazer kommen auch zwischendurch, aber ich habe keine Zeit und Lust mit essen zu gehen.
+
+19:00 - Brüderle macht sich auf den Heimweg, Mutti und Hotti auch.
+        Ich nehme Medis und lege mich im neuen Zimmer schlafen.
+        Etwas ungewohnt, ich kann aber gut einschlafen.
+
+
+===================================================================================================
+
+# Mon, 11.Mar.2024
+
+10:00 - Ich fühle mich immer noch wie gerädert.
+        Ich erstelle ein Support-Ticket für den WeChall Server. IPv6 funktioniert nicht.
+        Danach einkaufen... ich bin sparsam, vergesse aber etwas für das Mittagessen.
+        Ich wollte ja eh ein wenig abnehmen.
+
+13:00 - Ich verbringe den Tag mit Internet und YouTube, und mit etwas WeChall coding.
+        Ich implementiere eine Bildanzeige für Links in Chatnachrichten.
+
+20:00 - Ich gehe früh zu Bett.
+
+===================================================================================================
+
+# Tue, 12.Mar.2024
+
+13:00 - Ich werde spät wach, habe wieder geträumt aber ich erinnere mich nicht mehr genau.
+        Ich bin Flugzeug geflogen, und zum Schluss hat Otto-Waalkes Zaubertricks aufgeführt.
+
+14:00 - Ich gehe mit Marvin einkaufen.
+
+15:00 - Mutti und Hotti haben für heute abgesagt, und ich widme mich wieder dem Luca Projekt.
+
+
+===================================================================================================
+
+# Wed, 13.Mar.2024
+
+14:00 - Ich stehe wieder viel zu spät auf.
+        Mutti ist schon eingetroffen.
+
